a5a39a6a446777224f7952e7807f0e46228d0edb
Aditya Chemudupaty <achemudupaty@gmail.com>
2018-12-26 14:43:34 -0600

    Create while_loop_exercise.py
    created while_loop_exercises.py

---
+++ while_loop_exercise.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 26 14:38:49 2018
+
+@author: achem
+"""
+end = 4
+num = 1
+total = 0
+while num <= end:
+    total = num + total
+    num = num + 1
+print(total)
