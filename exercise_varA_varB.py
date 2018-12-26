0041af34ec8422b06742d467b6c9165daf3a3500
Aditya Chemudupaty <achemudupaty@gmail.com>
2018-12-26 14:26:05 -0600

    Create exercise_varA_varB.py
    created exercise_varA_varB.py, from Week 1: Python Basics

---
+++ exercise_varA_varB.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 26 14:20:36 2018
+
+@author: achem
+"""
+
+if type(varA) == str or type(varB) == str:
+    print("string involved")
+elif varA > varB:
+    print("bigger")
+elif varA == varB:
+    print("equal")
+elif varA < varB:
+    print("smaller")
