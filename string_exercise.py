59c266f420586f75826ecd119aadc779007af0af
Aditya Chemudupaty <achemudupaty@gmail.com>
2018-12-26 13:47:33 -0600

    Create string_exercise.py

---
+++ string_exercise.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 25 20:19:38 2018
+
+@author: achem
+"""
+
+if 6 > 7:
+    print("Yep")
+
+if 6 > 7:
+    print("Yep")
+else:
+    print("Nope")
+    
